6b3c5ef70d5c4ec2a086aacc462457f74933bbd1
Blake <blakemcmurrey@gmail.com>
2023-05-10 11:26:21 -0500

    Completed exercise

---
+++ VariablesExercise1/VariablesExercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string name = "Blake";
+int age = 19;
+char middleinit = 'W';
+bool isover18 = true;
+double currenttemp = 80.6;
+decimal currentprice = 19.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"age: {age}");
+Console.WriteLine($"middleinit: {middleinit}");
+Console.WriteLine($"isover18: {isover18}");
+Console.WriteLine($"currenttemp: {currenttemp}");
+Console.WriteLine($"currentprice: {currentprice}");
